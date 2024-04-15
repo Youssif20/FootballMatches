5a41c6dcbffae0694e8d6b325c146885a112efc9
Yousef.Wageh <youssif.wagih2020@gmail.com>
2024-04-15 14:55:00 +0200

    getting the title and league name

--- BeautifulSoup/main.py
+++ BeautifulSoup/main.py
@@ -1,3 +1,17 @@
 from bs4 import BeautifulSoup
 import requests
 
+# requested url
+url = "https://www.yallakora.com/match-center/"
+request = requests.get(url)
+content = request.text
+
+# parse the content
+soup = BeautifulSoup(content, 'html.parser')
+
+# find links
+soup.find('main', class_='cd-main-content')
+date = soup.find('div', class_='dayName').find('span').text
+numofmatches = soup.find('div', class_='toursMatchesNum').find('p').find('span', class_='matchesCount').text
+print(numofmatches)
+
